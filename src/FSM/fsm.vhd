83bb62b5ef90359e2fd7461dea4d313da6810e21
Alberto12MC <alberto12mc@gmail.com>
2020-04-16 23:30:42 +0200

    init fsm

---
+++ src/FSM/fsm.vhd
@@ -0,0 +1,36 @@
+-------------------------------------------------------
+--! @file  fsm.vhd
+--! @brief
+--! @todo
+--! @defgroup fsm
+-------------------------------------------------------
+
+--! Standard library.
+library ieee;
+--! Logic elements.
+use ieee.std_logic_1164.all;
+--! arithmetic functions.
+use ieee.numeric_std.all;
+
+--! @brief   implementation
+--! @details implementation of xxx
+--! @ingroup fsm
+
+entity fsm is
+  generic(
+
+  );
+  port (
+         -- Common
+         clk   : in std_logic;
+         reset : in std_logic;
+  );
+end fsm;
+
+architecture rtl of fsm is
+
+
+begin
+
+
+end rtl;
