c46e54fea189b1d5cfa2b68041ff38ad440c8b14
Alberto Martínez Cuesta <alberto12mc@gmail.com>
2020-04-19 17:24:49 +0200

    update fsm

--- src/FSM/fsm.vhd
+++ src/FSM/fsm.vhd
@@ -17,20 +17,46 @@ use ieee.numeric_std.all;
 --! @ingroup fsm
 
 entity fsm is
-  generic(
-
-  );
   port (
          -- Common
          clk   : in std_logic;
          reset : in std_logic;
+         -- Inputs
+         fsm_in_0 : in std_logic;
+         fsm_in_1 : in std_logic;
+         --Outputs
+         fsm_out_0 : out std_logic;
+         fsm_out_1 : out std_logic;
+         fsm_out_2 : out std_logic
   );
 end fsm;
 
 architecture rtl of fsm is
-
+type fsm_states_type is (idle, read1, read2);
+signal fsm_state: fsm_states_type;
 
 begin
+  fsm_process: process(clk, reset)
+  begin
+    if reset = '1' then
+      state <= S0;
+      Z <= '0';
+    elsif (rising_edge(clk)) then
+      case fsm_state is
+        when S0 =>
+          if X = '0' then
+            state <= S0;
+          elsif X = '1' then
+            state <= S1;
+          end if;
+            Z <= '0';
+        when S1 =>
 
+        when others =>
+          state <= idle;
+          Z <= '0';
+      end case;
+    end if;
+  end process;
 
 end rtl;
