032f650f3fa76e4294eb6372c2de49e015dc3a80
Alberto Martinez <alberto12mc@gmail.com>
2020-11-05 17:17:59 +0100

    Update fsm.vhd

--- src/FSM/fsm.vhd
+++ src/FSM/fsm.vhd
@@ -18,26 +18,26 @@ use ieee.numeric_std.all;
 
 entity fsm is
   port (
-         -- Common
-         clk   : in std_logic;
-         reset : in std_logic;
-         -- Inputs
-         fsm_in_0 : in std_logic;
-         fsm_in_1 : in std_logic;
-         --Outputs
-         fsm_out_0 : out std_logic;
-         fsm_out_1 : out std_logic
+    -- Common
+    clk       : in std_logic;
+    reset     : in std_logic;
+    -- Inputs
+    fsm_in_0  : in std_logic;
+    fsm_in_1  : in std_logic;
+    --Outputs
+    fsm_out_0 : out std_logic;
+    fsm_out_1 : out std_logic
   );
 end fsm;
 
 architecture rtl of fsm is
-type fsm_states_type is (IDLE, FREE_MODE, FIXED_MODE);
-signal fsm_state: fsm_states_type;
-attribute enum_encoding: string;
-attribute enum_encoding of fsm_states_type: type is "one_hot";
+  type fsm_states_type is (IDLE, FREE_MODE, FIXED_MODE);
+  signal fsm_state                           : fsm_states_type;
+  attribute enum_encoding                    : string;
+  attribute enum_encoding of fsm_states_type : type is "one_hot";
 
 begin
-  fsm_process: process(clk)
+  fsm_process : process (clk)
   begin
     if reset = '1' then
       fsm_state <= IDLE;
@@ -51,14 +51,14 @@ begin
           fsm_state <= FREE_MODE;
         when FREE_MODE =>
           fsm_out_0 <= '1';
-          if fsm_in_0='1' then
+          if fsm_in_0 = '1' then
             fsm_state <= FIXED_MODE;
           else
             fsm_state <= FREE_MODE;
           end if;
         when FIXED_MODE =>
           fsm_out_1 <= '1';
-          if fsm_in_1='1' then
+          if fsm_in_1 = '1' then
             fsm_state <= FREE_MODE;
           else
             fsm_state <= FIXED_MODE;
